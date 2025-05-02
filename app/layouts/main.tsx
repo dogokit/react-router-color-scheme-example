fc4f7845e3380fb9a6ef23a40c5721fa31480ed2
M Haidar Hanif <git@mhaidarhanif.com>
2025-05-03 03:27:16 +0700

    feat: setup shadcn ui components

--- app/layouts/main.tsx
+++ app/layouts/main.tsx
@@ -4,7 +4,7 @@ import { ColorSchemeToggle } from "~/components/shared/color-scheme-toggle";
 export default function MainLayout() {
   return (
     <div className="flex flex-col min-h-screen gap-10">
-      <header className="flex items-center justify-between px-6 py-4 bg-gray-100 dark:bg-gray-900">
+      <header className="flex items-center justify-between p-4">
         <ColorSchemeToggle action={href("/actions/color-scheme")} />
       </header>
 
